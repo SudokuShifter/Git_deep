81f7ed903ccc61122f69cf23456a982bcc6bc3b0
mr.Robot <zanid227@yandex.ru>
2024-07-17 13:03:02 +0300

    make 3 types reset

--- py_test.py
+++ py_test.py
@@ -1,11 +1,5 @@
 def some_func():
     print('Hello World')
 
-
 def some_func2():
     print('I love python')
-
-
-def some_func3(a: int, b: int):
-    print('Владислав, приветствую!')
-    return a + b
